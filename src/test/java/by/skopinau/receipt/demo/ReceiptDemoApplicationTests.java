89e0790223dd4949d345b5d39b26e933a095d360
Dzmitry Skopinau <dimaskop18@gmail.com>
2022-12-14 02:11:54 +0300

    Initial commit

---
+++ src/test/java/by/skopinau/receipt/demo/ReceiptDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package by.skopinau.receipt.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReceiptDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
